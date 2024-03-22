310c6c6c75a452061f6487e1dc7ad1f00b66e6c1
Caio Le Petit <cdct@gft.com>
2024-03-22 14:54:15 -0300

    feat: First commit

---
+++ src/main/java/br/com/fiap/pontoeletronicoregistro/PontoEletronicoRegistroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pontoeletronicoregistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoEletronicoRegistroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontoEletronicoRegistroApplication.class, args);
+    }
+
+}
